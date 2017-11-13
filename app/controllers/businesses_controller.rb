fae9d231722c9d148f861a5f8d726b488758a97c
Dawson Mortenson and Noah Gottlieb <noah@>
2017-11-13 11:54:45 -0800

    add CRUD

---
+++ app/controllers/businesses_controller.rb
@@ -0,0 +1,43 @@
+class BusinessesController < ApplicationController
+  def new
+    @type = Type.find(params[:type_id])
+    @business = @type.businesses.new
+  end
+
+  def create
+    @type = Type.find(params[:type_id])
+    @business = @type.businesses.new(business_params)
+    if @type.save
+      redirect_to type_path(@business.type)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @business = Business.find(params[:id])
+    @business.destroy
+    redirect_to type_path(@business.type)
+  end
+
+  def edit
+    @type = Type.find(params[:type_id])
+    @business = Business.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @business = Business.find(params[:id])
+    if @business.update(business_params)
+      redirect_to type_path(@business.type)
+    else
+      @type = Type.find(params[:type_id])
+      render :edit
+    end
+  end
+
+private
+  def business_params
+    params.require(:business).permit(:name,:telenum,:street,:city,:state,:zip,:email,:website)
+  end
+end
